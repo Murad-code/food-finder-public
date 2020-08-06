0998535a955b5a2656b4b392dc169d79976748be
Murad-code <murad.coding@gmail.com>
2020-08-06 12:33:14 +0100

    Completed favourites styling

--- client/src/FavouritesComponents/FavBusiness/FavBusiness.jsx
+++ client/src/FavouritesComponents/FavBusiness/FavBusiness.jsx
@@ -8,10 +8,14 @@ export const FavBusiness = (props) => {
 
   const getBusiness = async () => {
     console.log("in getBusiness");
-    const favourites = await axios.post("/favourites", { email: email });
-    const idList = favourites.data.favouritesId[0].favourites;
-    console.log(idList);
-    setBusiness(idList);
+    try {
+      const favourites = await axios.post("/favourites", { email: email });
+      const idList = favourites.data.favouritesId[0].favourites;
+      console.log(idList);
+      setBusiness(idList);
+    } catch (err) {
+      return;
+    }
   };
 
   const [business, setBusiness] = useState(() => getBusiness());
@@ -19,55 +23,45 @@ export const FavBusiness = (props) => {
   const BusinessComponent = (props) => {
     console.log("in BusinessComponent function");
     console.log("business length: " + business.length);
-    if (business.length === undefined) {
-      console.log("in if statement");
+    console.log("business state: " + business);
+    return business.map((business) => {
       return (
-        <div>
-          <h1>nothing</h1>
-        </div>
-      );
-    } else {
-      console.log("else statement");
-      console.log("business state: " + business);
-      return business.map((business) => {
-        return (
-          <div className="Business">
-            <div className="image-container">
-              <img src={business.imageSrc} alt="" />
+        <div className="Business">
+          <div className="image-container">
+            <img src={business.imageSrc} alt="" />
+          </div>
+          <div className="title-favourite">
+            <h2>{business.name}</h2>
+          </div>
+          <div className="Business-information">
+            <div className="Business-address">
+              <p>{business.address}</p>
+              <p>{business.city}</p>
+              <p>{business.zipCode}</p>
             </div>
-            <div className="title-favourite">
-              <h2>{business.name}</h2>
-            </div>
-            <div className="Business-information">
-              <div className="Business-address">
-                <p>{business.address}</p>
-                <p>{business.city}</p>
-                <p>{business.zipCode}</p>
-              </div>
-              <div className="Business-reviews">
-                <h3>{business.category}</h3>
-                <h3 className="rating">{business.rating}</h3>
-                <p>{business.reviewCount}</p>
-              </div>
+            <div className="Business-reviews">
+              <h3>{business.category}</h3>
+              <h3 className="rating">{business.rating}</h3>
+              <p>{business.reviewCount}</p>
             </div>
           </div>
-        );
-      });
-    }
+        </div>
+      );
+    });
   };
 
   const Empty = () => {
     console.log("In Empty function");
     return (
-      <div>
-        <h1>Empty</h1>
+      <div className="Empty">
+        <h1>Add favourites to see them here</h1>
       </div>
     );
   };
 
   return (
     <div id="Fav-Business" className="Fav-BusinessList">
-      <BusinessComponent />
+      {business.length !== undefined ? <BusinessComponent /> : <Empty />}
     </div>
   );
 };
